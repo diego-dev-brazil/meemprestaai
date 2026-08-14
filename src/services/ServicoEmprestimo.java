67b5b6f1b903af3ad70795505b470fa1806faa70
diego <diegoaugustooliveira3@gmail.com>
2026-08-14 09:21:02 -0300

    atualizacao servicoemprestimo

--- src/services/ServicoEmprestimo.java
+++ src/services/ServicoEmprestimo.java
@@ -1,15 +1,35 @@
 package services;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import entities.Emprestimo;
 import entities.Equipamento;
 import entities.Pessoa;
+import enum_entities.Status;
 
 public class ServicoEmprestimo {
+	private List<Emprestimo> emprestimos = new ArrayList<>();
 	public void realizarEmprestimo (Pessoa pessoa, Equipamento equipamento) {
 		LocalDateTime momento = LocalDateTime.now();
 		Emprestimo emprestimo = new Emprestimo (momento, momento.plusWeeks(1),pessoa,equipamento);
+		emprestimo.getEquipamento().setStatus(Status.EMPRESTADO);
+		emprestimos.add(emprestimo);
 		System.out.println("\nEmprestimo bem sucedido!");
+	}
+	public void realizarDevolucao (Emprestimo emprestimo ) {
+		if (!emprestimos.contains(emprestimo)) {
+	        System.out.println("Empréstimo não encontrado.");
+	        return;
+	    }
+
+	    if (emprestimo.getQuandoFoiDevolvido() != null) {
+	        System.out.println("Empréstimo já encerrado.");
+	        return;
+	    }
+
+	    emprestimo.getEquipamento().setStatus(Status.DISPONIVEL);
+	    emprestimo.setQuandoFoiDevolvido(LocalDateTime.now());
 	}
 }
